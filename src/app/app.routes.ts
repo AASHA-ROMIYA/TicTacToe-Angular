1a09422879c1e25f802ab546fc0db562bc478bb5
Aasha <aashasrinivasan64@gmail.com>
2025-12-23 13:21:11 +0530

    routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
-export const routes: Routes = [];
+const routes: Routes = [
+  // your routes here
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { useHash: true })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
